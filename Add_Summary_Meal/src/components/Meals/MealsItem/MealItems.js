4c00430662d535d732d074b9cb5d41a4fa702f0c
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-01-31 21:05:56 +0530

    Making Meal Item Visible

---
+++ Add_Summary_Meal/src/components/Meals/MealsItem/MealItems.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./MealItems.module.css";
+const MealItems = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+    </li>
+  );
+};
+
+export default MealItems;
